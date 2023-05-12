defd90e9ced6ee05e2a3bfb0ba0fe3072c4de356
Appointly <nocodeforu@gmail.com>
2023-05-11 22:09:04 -0400

    Add coming soon page

---
+++ resources/views/comingsoon.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Coming Soon</title>
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+            background-color: #f3f3f3;
+            font-family: Arial, sans-serif;
+        }
+        h1 {
+            font-size: 3em;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <h1>Coming Soon!</h1>
+</body>
+</html>
